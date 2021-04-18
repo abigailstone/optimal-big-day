18271dd5ec67cdd582fa160871ff8a0b3f1a2f21
Myles Stokowski <mylesstokowski@gmail.com>
2021-04-18 18:42:02 -0400

    generate species/hotspot probability matrix

---
+++ generate_probability_matrix.R
@@ -0,0 +1,25 @@
+# filtered data to species/hotspot probability matrix
+library(tidyverse)
+
+observations <- read_csv('filtered.csv')
+
+# for each location, calculate number of checklists and number of times each species has been observed
+checklists_per_loc <- observations %>% 
+   group_by(locality) %>%
+   summarize(n_checklists = n_distinct(checklist_id))
+
+counts_per_loc <- observations %>%
+   group_by(locality, common_name) %>%
+   summarize(n_sp_loc = n()) %>%
+   spread(key = common_name, value = n_sp_loc, fill = 0) %>%
+   right_join(checklists_per_hotspot, by = 'locality') %>%
+   #reorder the columns by moving n_checklists before inidividual speices counts
+   select('locality', 'n_checklists', 3:ncol(.))
+
+# counts to probabilities
+prob_per_loc <- counts_per_loc %>%
+   mutate_at(.vars = vars( 3:ncol(.)),
+             .funs = list(~ . / n_checklists)) %>%
+   ungroup
+
+write_csv(prob_per_loc, 'prob_per_loc.csv')
