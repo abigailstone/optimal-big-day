371099b2f7e24cced376125b69bff9ff39ec5ff1
Myles Stokowski <mylesstokowski@gmail.com>
2021-04-18 18:22:49 -0400

    minor tweaks

--- loaddata.R
+++ loaddata.R
@@ -4,6 +4,7 @@ library(auk)
 # assumes that ebd path has been set: 
 # auk_set_ebd_path(YOUR_EBD_PATH, overwrite=TRUE)
 input_file <- file.path('ebd_US-VT_201001_202011_relSep-2020.txt')
+# input_file <- file.path('ebd_US-VT-001_201103_202103_relMar-2021.txt') # this is the most recent data
 f_out <- 'ebd_filtered.txt'
 
 # auk filtering
@@ -18,13 +19,17 @@ cols <- c('category', 'common_name', 'observation_count',
          'county_code', 'locality', 'locality_id', 'locality_type',
          'latitude', 'longitude', 'observation_date', 'time_observations_started',
          'protocol_code', 'duration_minutes', 'effort_distance_km', 'all_species_reported',
-         'approved', 'reviewed')
+         'approved', 'reviewed', 'checklist_id')
 
 ebird_data_filtered <- ebird_data %>%
    # complete lists
    filter(all_species_reported==TRUE) %>% 
    # traveling or stationary
    filter(protocol_code %in% c('P21', 'P22')) %>%
-   select(all_of(cols))
+   select(cols) 
 
-write.csv(ebird_data_filtered, file='filtered.csv')
+write_csv(ebird_data_filtered, path='filtered.csv')
+
+
+
+
