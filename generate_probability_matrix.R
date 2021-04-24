6a44c5d0baa8ebe5ff502faca62d308ef0e6d022
Abigail Stone <stone.abigail@gmail.com>
2021-04-24 12:53:15 -0400

    filter out single-checklist locations

--- generate_probability_matrix.R
+++ generate_probability_matrix.R
@@ -5,15 +5,18 @@ observations <- read_csv('filtered.csv')
 
 # for each location, calculate number of checklists and number of times each species has been observed
 checklists_per_loc <- observations %>% 
+   filter(county_code == 'US-VT-001') %>%
    group_by(locality) %>%
-   summarize(n_checklists = n_distinct(checklist_id))
+   summarize(n_checklists = n_distinct(checklist_id)) %>% 
+   # filter locations with 1 checklist since all species will be 100% probability
+   filter(n_checklists > 1)
 
 counts_per_loc <- observations %>%
    group_by(locality, common_name) %>%
    summarize(n_sp_loc = n()) %>%
    spread(key = common_name, value = n_sp_loc, fill = 0) %>%
-   right_join(checklists_per_hotspot, by = 'locality') %>%
-   #reorder the columns by moving n_checklists before inidividual speices counts
+   right_join(checklists_per_loc, by = 'locality') %>%
+   #reorder the columns by moving n_checklists before individual species counts
    select('locality', 'n_checklists', 3:ncol(.))
 
 # counts to probabilities
