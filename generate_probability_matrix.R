962560eaf9fd05c00f64a9bd677d28fd9d3a44e2
Myles Stokowski <mylesstokowski@gmail.com>
2021-04-25 10:40:15 -0400

    effort variable graphing

--- generate_probability_matrix.R
+++ generate_probability_matrix.R
@@ -1,18 +1,33 @@
 # filtered data to species/hotspot probability matrix
 library(tidyverse)
+library(reshape2)
 
 observations <- read_csv('filtered.csv')
 
 # for each location, calculate number of checklists and number of times each species has been observed
-checklists_per_loc <- observations %>% 
-   group_by(locality) %>%
-   summarize(n_checklists = n_distinct(checklist_id))
+effort_per_loc <- observations %>% 
+   distinct(checklist_id, .keep_all = TRUE) %>%
+   group_by(locality) %>% 
+   summarize(
+      n_checklists = n(),
+      total_time = sum(duration_minutes),
+      med_time = median(duration_minutes),
+      iqr_time = IQR(duration_minutes),
+      total_distance = sum(effort_distance_km)
+   ) %>% 
+   mutate(time_per_checklist = total_time / n_checklists)
+
+# graphing 
+effort_tall <- melt(effort_per_loc, id.vars="locality")
+effort_tall %>% ggplot(aes(value)) + 
+   geom_density() + 
+   facet_wrap(~variable, scales = "free")
 
 counts_per_loc <- observations %>%
    group_by(locality, common_name) %>%
    summarize(n_sp_loc = n()) %>%
    spread(key = common_name, value = n_sp_loc, fill = 0) %>%
-   right_join(checklists_per_hotspot, by = 'locality') %>%
+   right_join(effort_per_loc, by = 'locality') %>%
    #reorder the columns by moving n_checklists before inidividual speices counts
    select('locality', 'n_checklists', 3:ncol(.))
 
