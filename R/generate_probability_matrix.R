5e70f0e7d566fd128d25a1711b698f203ab368ad
Myles Stokowski <mylesstokowski@gmail.com>
2021-04-30 12:15:30 -0400

    clean prob_matrix code - output change is just reordered columns

--- R/generate_probability_matrix.R
+++ R/generate_probability_matrix.R
@@ -2,49 +2,71 @@
 library(tidyverse)
 library(reshape2)
 
-observations <- read_csv('data/filtered.csv')
-
-# for each location, calculate number of checklists and number of times each species has been observed
-effort_per_loc <- observations %>% 
-   filter(county_code == 'US-VT-001') %>%
-   mutate(effort_distance_km = ifelse(is.na(effort_distance_km), 0, effort_distance_km)) %>% # handle stationary lists
-   distinct(checklist_id, .keep_all = TRUE) %>%
-   group_by(locality) %>% 
-   summarize(
-      n_checklists = n(),
-      total_time = sum(duration_minutes),
-      med_time = median(duration_minutes),
-      iqr_time = IQR(duration_minutes),
-      total_distance = sum(effort_distance_km)
-   ) %>% 
-   mutate(time_per_checklist = total_time / n_checklists) %>% 
-   filter(n_checklists > 1)
+# return tibble with effort information for each hotspot
+effort_per_hotspot <- function(observations) {
+   observations %>% 
+      # filter(county_code == 'US-VT-001') %>%
+      mutate(effort_distance_km = ifelse(is.na(effort_distance_km), 0, effort_distance_km)) %>% # handle stationary lists
+      distinct(checklist_id, .keep_all = TRUE) %>%
+      group_by(locality) %>% 
+      summarize(
+         n_checklists = n(),
+         total_time = sum(duration_minutes),
+         med_time = median(duration_minutes),
+         iqr_time = IQR(duration_minutes),
+         total_distance = sum(effort_distance_km)
+      ) %>% 
+      mutate(time_per_checklist = total_time / n_checklists) %>% 
+      filter(n_checklists > 1)
+}
 
 # graphing effort variables
-effort_tall <- melt(effort_per_loc, id.vars="locality")
-effort_tall %>% ggplot(aes(value)) + 
-   geom_density() + 
-   facet_wrap(~variable, scales = "free")
+# effort_tall <- melt(effort_per_loc, id.vars="locality")
+# effort_tall %>% ggplot(aes(value)) + 
+#    geom_density() + 
+#    facet_wrap(~variable, scales = "free")
 
 # does total time correlate with number of checklists for each hotspot?
-effort_per_loc %>% 
-   ggplot(aes(n_checklists, total_time)) + 
-   geom_point() + 
-   stat_smooth()
+# effort_per_loc %>% 
+#    ggplot(aes(n_checklists, total_time)) + 
+#    geom_point() + 
+#    stat_smooth()
 
-# counts of checklists per hotspot
-counts_per_loc <- observations %>%
-   group_by(locality, common_name) %>%
-   summarize(n_sp_loc = n()) %>%
-   spread(key = common_name, value = n_sp_loc, fill = 0) %>%
-   right_join(effort_per_loc, by = 'locality') %>%
-   # reorder the columns by moving effort vars before inidividual speices counts
-   select(colnames(effort_per_loc), length(colnames(effort_per_loc)):ncol(.))
 
-# counts to probabilities
-prob_per_loc <- counts_per_loc %>%
-   mutate_at(.vars = vars(length(colnames(effort_per_loc)):ncol(.)),
-             .funs = list(~ . / n_checklists)) %>%
-   ungroup
 
-write_csv(prob_per_loc, 'data/prob_per_loc.csv')
+# return a tibble with number of checklists reporting each species at each hotspot
+n_observations_per_hotspot <- function(observations) {
+   observations %>%
+      group_by(locality, common_name) %>%
+      summarize(n_sp_loc = n(), .groups = "drop") %>%
+      spread(key = common_name, value = n_sp_loc, fill = 0)
+}
+
+probability_matrix <- function(observations) {
+   
+   effort_per_loc <- effort_per_hotspot(observations)
+   counts_per_loc <- n_observations_per_hotspot(observations)
+   
+   # join species frequency and effort information
+   counts_and_effort_per_loc <- effort_per_loc %>%
+      left_join(counts_per_loc, by = "locality")   
+   
+   counts_and_effort_per_loc %>% 
+      mutate_at(
+         
+         # for only species columns (exclude locality and effort columns)
+         .vars = setdiff(colnames(counts_and_effort_per_loc), 
+                                  colnames(effort_per_loc)),
+                
+          # divide by the number of checklists at that location
+         .funs = list(~ . / n_checklists))
+}
+
+# turning this off so that sourcing the file just loads the functions
+if (FALSE) {
+   observations <- read_csv('data/filtered.csv')
+   prob_per_loc <- probability_matrix(observations)
+   write_csv(prob_per_loc, 'data/prob_per_loc.csv')
+}
+
+
