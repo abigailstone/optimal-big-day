965709d799affc0a2703c342ea28eb729cebf802
Myles Stokowski <mylesstokowski@gmail.com>
2021-05-05 18:58:58 -0400

    loaddata tweaks - do more using auk - removed some redundant filtering

--- R/loaddata.R
+++ R/loaddata.R
@@ -1,53 +1,48 @@
 library(tidyverse)
 library(auk)
 
-# assumes that ebd path has been set: 
-# auk_set_ebd_path(YOUR_EBD_PATH, overwrite=TRUE)
+if (is.na(auk_get_ebd_path())) {
+   stop("please set ebd path with auk_set_ebd_path(YOUR_EBD_PATH)")
+}
+
 input_file <- file.path('ebd_US-VT_201001_202011_relSep-2020.txt')
 # input_file <- file.path('ebd_US-VT-001_201103_202103_relMar-2021.txt') # this is the most recent data
 f_out <- 'data/ebd_filtered.txt'
+
+# columns to keep for filtering
+cols <- c('group_identifier', 'sampling_event_identifier', 'observer_id',
+          'scientific_name', 'observation_count', 'category', 'common_name',
+          'county_code', 'locality', 'locality_id', 'locality_type',
+          'latitude', 'longitude', 'observation_date', 'protocol_code', 
+          'time_observations_started', 'duration_minutes', 'effort_distance_km', 
+          'all_species_reported', 'approved', 'reviewed', 'state', 'county')
 
 # auk filtering
 ebird_data <- input_file %>%
    auk_ebd() %>%
    auk_complete() %>%
    auk_date(date = c('*-05-01', '*-05-15')) %>%
-   auk_filter(file = f_out, overwrite = TRUE) %>%
+   auk_protocol(c("Stationary", "Traveling")) %>% 
+   auk_filter(file = f_out, overwrite = TRUE, keep = cols) %>%
    read_ebd()
 
-# columns to keep for filtering
-cols <- c('category', 'common_name', 'observation_count',
-         'county_code', 'locality', 'locality_id', 'locality_type',
-         'latitude', 'longitude', 'observation_date', 'time_observations_started',
-         'protocol_code', 'duration_minutes', 'effort_distance_km', 'all_species_reported',
-         'approved', 'reviewed', 'checklist_id', 'state', 'county')
-
+# filtering that auk cannot do 
 ebird_data_filtered <- ebird_data %>%
-   
-   filter(all_species_reported==TRUE, # complete lists
-          protocol_code %in% c('P21', 'P22'), # travelling or stationary
-          category == "species", # omit sp. %>%
-          observation_count != "0",  # "X" is fine, we just want to know presence
-          locality_type == "H",
-          
-          ) %>% 
-   select(all_of(cols))
-
-write_csv(ebird_data_filtered, 'data/filtered.csv')
-
+   filter(locality_type == "H") %>%
+   write_csv('data/filtered.csv')
 
 # county list 
-countycodes <- ebird_data %>% 
+countycodes <- ebird_data_filtered %>% 
    group_by(state, county) %>% 
    summarise(county_code = first(county_code)) %>% 
    write_csv('data/counties.csv')
 
 # hotspot list 
-loc_list <- ebird_data %>% 
-   filter(locality_type == "H") %>%
+loc_list <- ebird_data_filtered %>% 
    group_by(county_code, locality) %>% 
    summarise(latitude = first(latitude),
              longitude = first(longitude)) %>%
    write_csv('data/hotspots.csv')
 
-
+# clear global variables 
+rm(list = ls())
