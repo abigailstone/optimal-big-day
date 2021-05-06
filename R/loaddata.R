de413048ac00c174593b1e419c15a509af255aff
Abigail Stone <stone.abigail@gmail.com>
2021-05-05 22:18:34 -0400

    load_data_main function, also support for multiple data sets!!

--- R/loaddata.R
+++ R/loaddata.R
@@ -5,43 +5,63 @@ if (is.na(auk_get_ebd_path())) {
    stop("please set ebd path with auk_set_ebd_path(YOUR_EBD_PATH)")
 }
 
-input_file <- file.path('ebd_US-VT_201103_202103_relMar-2021.txt')
-# input_file <- file.path('ebd_US-VT-001_201103_202103_relMar-2021.txt') # this is the most recent data
-f_out <- 'data/ebd_filtered.txt'
+files <- list.files(auk_get_ebd_path(), pattern='*.txt')
 
-# columns to keep for filtering
-cols <- c('group_identifier', 'sampling_event_identifier', 'observer_id',
-          'scientific_name', 'observation_count', 'category', 'common_name',
-          'county_code', 'locality', 'locality_id', 'locality_type',
-          'latitude', 'longitude', 'observation_date', 'protocol_code', 
-          'time_observations_started', 'duration_minutes', 'effort_distance_km', 
-          'all_species_reported', 'approved', 'reviewed', 'state', 'county')
+# load all of the data in the auk_ebd path 
+load_data_main <- function(files){
+   
+   ebd_data <- NULL
+   f_out <- 'data/ebd_filtered.txt'
+   
+   # columns to keep for filtering
+   cols <- c('group_identifier', 'sampling_event_identifier', 'observer_id',
+             'scientific_name', 'observation_count', 'common_name',
+             'county_code', 'locality', 'locality_id', 'locality_type',
+             'latitude', 'longitude', 'observation_date', 'protocol_code', 
+             'time_observations_started', 'duration_minutes', 'effort_distance_km', 
+             'all_species_reported', 'state', 'county')
+   
+   for (f in files){
+      
+      # auk filtering
+      data <- f %>%
+         auk_ebd() %>%
+         auk_complete() %>%
+         auk_date(date = c('*-05-01', '*-05-15')) %>%
+         auk_protocol(c("Stationary", "Traveling")) %>% 
+         auk_filter(file = f_out, overwrite = TRUE, keep = cols) %>%
+         read_ebd()
+      
+      # filtering that auk cannot do 
+      filtered <- data %>%
+         filter(locality_type == "H")
+      
+      # join to existing data 
+      if (is.null(ebd_data)){
+         ebd_data <- filtered
+      } else {
+         ebd_data <- rbind(ebd_data, filtered)
+      }
+      
+   }
+   
+   # save the filtered data
+   write_csv(ebd_data, 'data/filtered.csv')
+   
+}
 
-# auk filtering
-ebird_data <- input_file %>%
-   auk_ebd() %>%
-   auk_complete() %>%
-   auk_date(date = c('*-05-01', '*-05-15')) %>%
-   auk_protocol(c("Stationary", "Traveling")) %>% 
-   auk_filter(file = f_out, overwrite = TRUE, keep = cols) %>%
-   read_ebd()
-
-# filtering that auk cannot do 
-ebird_data_filtered <- ebird_data %>%
-   filter(locality_type == "H") %>%
-   write_csv('data/filtered.csv')
 
 # county list 
-get_county_list <- function(){
-   countycodes <- ebird_data_filtered %>% 
+get_county_list <- function(data){
+   countycodes <- data %>% 
       group_by(state, county) %>% 
       summarise(county_code = first(county_code)) %>% 
       write_csv('data/counties.csv') 
 }
 
 # hotspot list 
-get_hotspot_list <- function(){
-   loc_list <- ebird_data_filtered %>% 
+get_hotspot_list <- function(data){
+   loc_list <- data %>% 
       group_by(county_code, locality) %>% 
       summarise(latitude = first(latitude),
                 longitude = first(longitude)) %>%
@@ -51,8 +71,11 @@ get_hotspot_list <- function(){
 
 
 if(FALSE){
-   get_county_list()
-   get_hotspot_list()
+   load_data_main(files)
+   
+   data <- read_csv('data/filtered.csv')
+   get_county_list(data)
+   get_hotspot_list(data)
 }
 
 # clear global variables 
