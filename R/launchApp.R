719e4656ceec48083d0d22ac5ac3ae9d8ff12aa5
Myles Stokowski <mylesstokowski@gmail.com>
2021-06-13 14:00:27 -0400

    delete everything

--- R/launchApp.R
+++
@@ -1,9 +0,0 @@
-#' launches the optimalBigDay shiny app
-#'
-#' @export launchApp
-#'
-#' @return shiny application object
-#' 
-launchApp <- function() {
-   shiny::shinyApp(ui = ui(), server = server)
-}
--- inst/shinyApp/app.R
+++
@@ -1,1 +0,0 @@
-optimalBigDay::launchApp()
