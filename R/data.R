a5310cbb0bf7fcf84b0a2e60c17df72dd937b708
Myles Stokowski <mylesstokowski@gmail.com>
2021-06-15 11:19:13 -0400

    add data documentation

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' sample eBird observation data for Addison County, VT, US, during April 2020
+#'
+#' @source \url{https://ebird.org/data/download/ebd}
+"sample_observations"
+
+#' sample county list, containing just Addison County
+"sample_counties"
+
+#' sample list of hotspots in Addison County
+"sample_hotspots"
+
+#' sample tibble of probability of seeing each species at each hotspot
+#' in Addison County, calculated from sample_observations
+"sample_prob_per_loc"
